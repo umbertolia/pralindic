656649096b583cd35ebace3f2827013254fd7a41
umbertolia <bertik@sfr.fr>
2019-07-14 19:15:12 +0200

    Init partie Courses

--- src/app/food-manager/food-manager.component.ts
+++ src/app/food-manager/food-manager.component.ts
@@ -1,26 +1,48 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Category} from '../models/category.model';
-import {Subscription} from 'rxjs';
 import {CategoryService} from '../services/category.service';
+import {Food} from '../models/food.model';
+import {FoodService} from '../services/food.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-food-manager',
   templateUrl: './food-manager.component.html',
   styleUrls: ['./food-manager.component.scss']
 })
-export class FoodManagerComponent implements OnInit {
+export class FoodManagerComponent implements OnInit, OnDestroy {
 
   categories: Map<string, Category>;
-  categoriesSubscription: Subscription;
+  foods: Map<string, Food>;
+  selectedFoods: string[];
+  panelsOpened: Map<string, boolean>;
 
-  constructor(private categoryService: CategoryService) { }
+  constructor(private categoryService: CategoryService,
+              private foodService: FoodService) { }
 
   ngOnInit() {
-    this.categoriesSubscription = this.categoryService.categoriesSubject.subscribe(
-    (categoryList: Map<string, Category>) => {
-      this.categories = categoryList;
-    });
+    this.getCategoriesAndFoods();
+    this.panelsOpened = new Map<string, boolean>();
+  }
+
+  private getCategoriesAndFoods() {
+    of(this.categoryService.categoriesSubject.subscribe(
+      (categoryList: Map<string, Category>) => {
+        this.categories = categoryList;
+      }));
+    of(this.foodService.foodsSubject.subscribe(
+      (foodList: Map<string, Food>) => {
+        this.foods = foodList;
+      }));
     this.categoryService.fetchCategories();
+    this.foodService.fetchFoods();
+  }
+
+  managerFoodsCard() {
+    console.log('foods selected size : ' + this.selectedFoods.length);
+  }
+
+  ngOnDestroy() {
   }
 
 }
